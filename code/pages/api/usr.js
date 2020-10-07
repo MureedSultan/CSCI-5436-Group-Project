5d7d9ba5b68102ea48cabe45c92c1a6b20f3166a
Mureed Sultan <sultan.0313@gmail.com>
2020-10-07 14:17:43 -0400

    Auth & DB
    * Added NextJS Auth
    * Added MySQL DB

---
+++ code/pages/api/usr.js
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+const password = 'ms13797';
+const rounds = 10;
+
+const hashed = '$2b$10$E2CEtpBnO/C2JAYauJQmV..3IDyJ1.Y/nzdtnM4t3ZR//TWxR1Zxm';
+
+const prisma = new PrismaClient();
+
+export default async function handle(req, res) {
+  // const users = await prisma.usr.findMany();
+  await bcrypt.hash(password, rounds, (err, hash) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(hash);
+  });
+  console.log(await bcrypt.compare(password, hashed));
+  res.json(true);
+}
